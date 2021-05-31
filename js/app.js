68a498afaeaf0e44ab2e0ae3a9ad9b8a75945552
Deepan Chakravarthi <39833033+Darkcodelab@users.noreply.github.com>
2021-05-31 16:31:54 +0530

    Delete app.js

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const menu__div = document.querySelector(".menu");
-const nav__ul = document.querySelector(".nav-ul");
-
-menu__div.addEventListener("click", () => {
-  nav__ul.classList.toggle("nav-open");
-});
